012b5dcc4c15ffc725e742d5fc4dd6c89b80cdf7
gitkizzm <github@tri-b-oon.de>
2023-11-16 01:02:06 +0100

    Dockerized
    Dockerstruckture + File

---
+++ src/main.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 15 22:14:26 2023
+
+@author: xbox
+
+https://www.youtube.com/watch?v=0TFWtfFY87U
+https://www.back4app.com/docs-containers/deployment-process
+"""
+
+import uvicorn
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def central_function():
+    return{ "eins":"1" }
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, port=8000, host="0.0.0.0")
